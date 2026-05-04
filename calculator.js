70dc1fe1dc67d67e6eee86a69f3d495842d43742
Pranathi <kankalapranathi661@gmail.com>
2026-05-04 14:26:46 +0530

    frstcommit

---
+++ calculator.js
@@ -0,0 +1,14 @@
+let num1=10
+let num2=20
+let operator='+';
+switch(operator){
+    case "+":
+        console.log(num1+num2)
+    case "-":
+        console.log(num1-num2)
+    case "*":
+        console.log(num1*num2)
+    case "/":
+        console.log(num1/num2)
+    
+}
---
+++ first.js
@@ -0,0 +1,7 @@
+const number=10;
+if(number%2===0){
+    console.log("Even no")
+}
+else{
+    console.log("Odd no")
+}
